e24b5ebc28b51f92c42637265f27d2835885e31d
Mina-cy <minachenyu08@gmail.com>
2026-03-11 13:11:27 +0800

    fix:
    1. news頁面細節（日期部分原本沒對齊，加上shrink-0）

--- src/components/NewActivities.vue
+++ src/components/NewActivities.vue
@@ -42,15 +42,15 @@ const cardStyle = computed(() => {
   >
     <div class="flex h-full items-center">
       <div
-        class="ml-3 flex h-[50px] w-[120px] shrink-0 md:w-[150px] items-center justify-center  text-[14px] sm:h-[75px] sm:text-[18px] md:text-xl lg:mr-6 lg:text-2xl xl:text-2xl"
+        class="ml-3 flex h-[50px] w-[90px] shrink-0 md:w-[150px] items-center justify-center  text-[14px] sm:h-[75px] sm:text-[18px] md:text-xl lg:mr-6 lg:text-2xl xl:text-2xl"
          :class="[cardStyle.bg, cardStyle.text]"
       >
         {{ cardStyle.label }}
       </div>
       <div
-        class="mr-3 w-[90px] border-r-2 border-gray-700 px-4 md:w-[120px] lg:mr-6 lg:w-[150px]"
+        class="mr-3 w-[90px] border-r-2 border-gray-700 px-4 md:w-[120px] lg:mr-6 lg:w-[140px] shrink-0"
       >
-        <p class="text-[10px]">{{ year }}</p>
+        <p class="text-[10px] md:text-[16px]">{{ year }}</p>
         <p class="text-xl font-bold md:text-3xl lg:text-3xl xl:text-4xl">
           {{ date }}
         </p>
